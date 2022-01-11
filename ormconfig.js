5b5c5e2071a9dbfd82b9b9907fafbf99f252cf22
alexandrejastrow <alexandre.jastrow@gmail.com>
2022-01-11 08:46:58 -0300

    initial configs

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "type": "postgres",
+    "url": process.env.DATABASE_URL,
+    "entities": [
+        "src/app/models/*.ts",
+
+    ],
+    "migrations": [
+        "src/database/migrations/*.ts"
+    ],
+    "cli": {
+        "migrationsDir": "src/database/migrations"
+    }
+}
+
+/*
+
+    "ssl": true,
+    "synchronize": true,
+    "extra": {
+        "ssl": {
+            "rejectUnauthorized": false
+        }
+    },
+*/
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from "express"
+
+
+const app = express()
+
+
+app.listen(process.env.PORT || "3000", () => {
+    console.log("server on")
+})
